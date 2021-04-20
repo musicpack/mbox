3d20f77fdd4fb13ed47135f704a0f389407a5460
Boris Liao <borisliaos@gmail.com>
2021-04-20 18:14:52 -0400

    Incremented Version Number

--- src/constants.py
+++ src/constants.py
@@ -4,7 +4,7 @@ import shutil
 import glob
 
 ###### PROGRAM CONSTANTS ######
-VERSION = '0.4'
+VERSION = '0.5'
 YOUTUBE_ICON = 'https://yt3.ggpht.com/a/AATXAJxHHP_h8bUovc1qC4c07sVXxVbp3gwDEg-iq8gbFQ=s100-c-k-c0xffffffff-no-rj-mo'
 USAGE_TEXT = 'Send a search phrase or a link to play a song!'
 CONFIG_PATH = 'config.ini'
