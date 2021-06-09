b5a458eb61faff1dd716f3f45d31315998e7672f
Boris Liao <borisliaos@gmail.com>
2021-06-09 15:19:05 -0400

    Changed Lyrics to follow arch diagram

---
+++ src/music/lyrics.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+from ytmusicapi import YTMusic
+
+def youtube_lyrics(youtube_id: str) -> Tuple[str, str]:
+    """Gets lyrics (if exists) given the youtube id.
+    NOTE: get_lyrics operation is wasteful as it discards a watch playlist while getting lyrics."""
+    ytmusic = YTMusic()
+    # TODO: Make more efficent use of watch_playlist
+    watch_playlist = ytmusic.get_watch_playlist(videoId=youtube_id)
+
+    browse_id = watch_playlist['lyrics']
+    if browse_id:
+        result = ytmusic.get_lyrics(browse_id)
+        return result['lyrics'], result['source']
+
+    return None
