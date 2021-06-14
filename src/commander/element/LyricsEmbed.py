4d2174242d420d6b49f952f0c3216b66fa0839d6
Jeremiah Jihoon Park <joon7853@gmail.com>
2021-06-14 13:50:25 -0400

    Lyric > 2048 chars fixed (with gaps only)

--- src/commander/element/LyricsEmbed.py
+++ src/commander/element/LyricsEmbed.py
@@ -23,20 +23,41 @@ class LyricsEmbed(Embed):
             lyric = ''
             lyric_text = ''
             lyric_list = self.lyrics.split('\r\n\r\n')
-            for lyric_line in lyric_list:
-                if(lyric_text == ''):
-                    if(len(lyric) + len(lyric_line) < 2048 ):
-                        lyric = lyric + '\r\n\r\n' + lyric_line
+            
+            #Lyric with gaps
+            if(len(lyric_list) > 1):
+                for lyric_line in lyric_list:
+                    if(lyric_text == ''):
+                        if(len(lyric) + len(lyric_line) < 2048 ):
+                            lyric = lyric + '\r\n\r\n' + lyric_line
+                        else:
+                            self.description = lyric
+                            lyric_text = lyric_line
                     else:
-                        self.description = lyric
-                        lyric_text = lyric_line
-                else:
-                    if(len(lyric_text) + len(lyric_line) < 1024 ):
-                        lyric_text = lyric_text + '\r\n\r\n' + lyric_line
+                        if(len(lyric_text) + len(lyric_line) < 1024 ):
+                            lyric_text = lyric_text + '\r\n\r\n' + lyric_line
+                        else:
+                            self.add_field(name='\u200B', value=lyric_text, inline=False)
+                            lyric_text = lyric_line
+
+            #Lyric without gap
+            elif(len(lyric_list) == 1):
+                lyric_list = self.lyrics.split('\r\n')
+                for lyric_line in lyric_list:
+                    if(lyric_text == ''):
+                        if(len(lyric) + len(lyric_line) < 2048 ):
+                            lyric = lyric + '\r\n' + lyric_line
+                        else:
+                            self.description = lyric
+                            lyric_text = lyric_line
                     else:
-                        self.add_field(name='\u200B', value=lyric_text, inline=False)
-                        lyric_text = lyric_line
-            self.add_field(name='\u200B', value=lyric_text, inline=False)
+                        if(len(lyric_text) + len(lyric_line) < 1024 ):
+                            lyric_text = lyric_text + '\r\n' + lyric_line
+                        else:
+                            self.add_field(name='\u200B', value=lyric_text, inline=False)
+                            lyric_text = lyric_line
+                    self.add_field(name='\u200B', value=lyric_text, inline=False)
+                    
         if self.lyrics_source:
             self.set_footer(text=self.lyrics_source)
         else:
