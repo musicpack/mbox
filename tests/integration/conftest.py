ec0d60fc0511834a47610f9af15e503e91fb3a7d
Boris Liao <borisliaos@gmail.com>
2021-06-15 21:43:46 -0400

    Add pytest integration test using pytest-xprocess

---
+++ tests/integration/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import sys
+import os
+from xprocess import ProcessStarter
+
+
+@pytest.fixture(scope="module")
+def mbox_process(xprocess):
+    main_path = os.path.join(pytest.__pytestPDB._config.rootdir,'main.py')
+    class Starter(ProcessStarter):
+        python_executable_full_path = sys.executable
+
+        timeout = 20
+        max_read_lines = 150
+        # startup pattern
+        pattern = "Music Box is all set up and ready to go!"
+
+        # command to start process
+        args = [python_executable_full_path, main_path, 'debug']
+
+    # ensure process is running and return its logfile
+    logfile = xprocess.ensure("mbox_process", Starter)
+
+    yield logfile
+
+    # clean up whole process tree afterwards
+    xprocess.getinfo("mbox_process").terminate()
+    
---
+++ tests/integration/test_main.py
@@ -0,0 +1,6 @@
+import pytest
+import time
+
+def test_bot_on_ready(mbox_process):
+    # Pytest loads mbox_progress from conftest.py
+    assert mbox_process
