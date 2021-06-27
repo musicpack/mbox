87053c9d98fb25288901c0cc05348f55f0462e01
Eric Wong <38166320+Exsects@users.noreply.github.com>
2021-06-27 00:09:59 -0400

    add flake8 and black as dependencies

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ slash = SlashCommand(bot, sync_commands=True)
 
 watching_channels = []
 
-COMMAND_CHANNEL_WARNING = 'Accepted command.'
+COMMAND_CHANNEL_WARNING = "Accepted command."
 
 logging_level = logging.INFO
 if len(sys.argv) > 1:
-    if sys.argv[1] == 'debug':
+    if sys.argv[1] == "debug":
         logging_level = logging.DEBUG
 
 logging.basicConfig(
     level=logging_level,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("debug.log", encoding='utf8'),
-        logging.StreamHandler()
-    ]
+        logging.FileHandler("debug.log", encoding="utf8"),
+        logging.StreamHandler(),
+    ],
 )
 
-initial_extensions = ['event_listener', 'music_controller']
+initial_extensions = ["event_listener", "music_controller"]
 
 for filename in initial_extensions:
     bot.load_extension(f"cogs.{filename}")
