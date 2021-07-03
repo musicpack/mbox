e880a97f79fe39e236facddec415c815e734274a
Boris Liao <borisliaos@gmail.com>
2021-07-03 17:14:53 -0400

    Moved DynamoDB file to element folder

--- cogs/event_listener.py
+++ cogs/event_listener.py
@@ -3,21 +3,20 @@ from typing import List, Union
 
 import discord
 from discord.ext import commands
-from src.lib import dynamodb
+
 import src.preinitialization
 from config import TOKEN
 from main import bot, logging
 from src.auth import Auth, Crypto
 from src.command_handler import play_ytid
-from src.constants import INVITE_LINK_FORMAT
+from src.element.dynamodb import Dynamodb
 from src.element.MusicBoxContext import MusicBoxContext
-from src.element.profile import Profile
 from src.parser import parse
 
 COMMAND_CHANNEL_WARNING = "Accepted command."
 watching_channels = []
 profiles: List[src.element.profile.Profile] = []
-dynamoDB = dynamodb.Dynamodb()
+dynamoDB = Dynamodb()
 
 
 class EventListener(commands.Cog):
@@ -48,25 +47,26 @@ class EventListener(commands.Cog):
         if message.author == bot.user:
             return
 
-        # check if item already exists in memory 
+        # check if item already exists in memory
         requested_item = dynamoDB.guild_item_exists_in_memory(self.bot.user.id)
 
         # Retreve guild item from database using application id if item does not exists in memory
-        if(not requested_item):
-            get_response: dict = dynamoDB.retrieve_guild_item(self.bot.user.id),
-            logging.info(f'printing get response from dynamo: {get_response}')
+        if not requested_item:
+            get_response: dict = (
+                dynamoDB.retrieve_guild_item(self.bot.user.id),
+            )
+            logging.info(f"printing get response from dynamo: {get_response}")
         else:
             logging.info("getting info from in memory object")
-            logging.info(f'Object in memory: {requested_item}')
+            logging.info(f"Object in memory: {requested_item}")
 
+        # TODO: still using profiles right now (will be removed once MusicBoxContext is refactored)
 
-        #TODO: still using profiles right now (will be removed once MusicBoxContext is refactored)
-        
         # Check which profile the message relates to
         for profile in profiles:
             if profile.guild == message.guild:
-                
-                #TODO Check if its from a command channel 
+
+                # TODO Check if its from a command channel
                 # Check if the message comes from a command channel
                 if profile.command_channel == message.channel:
                     await message.delete()
@@ -137,18 +137,21 @@ class EventListener(commands.Cog):
     async def on_guild_join(self, guild: discord.Guild):
 
         # Adding guild item to database first time bot joins server
-        put_response: dict = dynamoDB.store_guild_item(self.bot.user.id , guild.id,)
-        logging.info(f'Printing response from Dynamo: {put_response}')
-        logging.info(f'Joined Server: {guild.name}')
-        await guild.text_channels[0].send('Thanks for adding Music Bot!')
+        put_response: dict = dynamoDB.store_guild_item(
+            self.bot.user.id,
+            guild.id,
+        )
+        logging.info(f"Printing response from Dynamo: {put_response}")
+        logging.info(f"Joined Server: {guild.name}")
+        await guild.text_channels[0].send("Thanks for adding Music Bot!")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
 
-        #Remove guild item from database
+        # Remove guild item from database
         delete_response: dict = dynamoDB.delete_guild_item(self.bot.user.id)
-        logging.info(f'Printing response from Dynamo: {delete_response}')
-        logging.info('Removed from Server: {0.name}'.format(guild))
+        logging.info(f"Printing response from Dynamo: {delete_response}")
+        logging.info("Removed from Server: {0.name}".format(guild))
 
     @commands.Cog.listener()
     async def on_reaction_add(
--- src/element/dynamodb.py
+++ src/element/dynamodb.py
@@ -1,20 +1,22 @@
-import boto3
 import logging
+
+import boto3
 from botocore.exceptions import ClientError
 
 
 class Dynamodb:
     def __init__(self):
         dynamodb = boto3.resource(
-            service_name='dynamodb', region_name="us-east-2")
-        self.table = dynamodb.Table('music-box-db')
+            service_name="dynamodb", region_name="us-east-2"
+        )
+        self.table = dynamodb.Table("music-box-db")
         self.guild_items = []
 
     def store_guild_item(self, application_id: int, guild_id: int) -> dict:
         """Takes in a application id as the primary key and creates an object for the guild in dynamodb
 
         Args:
-            application_id (int): The id of the bot/application 
+            application_id (int): The id of the bot/application
             guild_id (int): The id of the guild/server that the bot is in
 
         Returns:
@@ -22,9 +24,9 @@ class Dynamodb:
         """
         response = self.table.put_item(
             Item={
-                'application_id': application_id,
-                'guild_id': guild_id,
-                'volume_level': 50
+                "application_id": application_id,
+                "guild_id": guild_id,
+                "volume_level": 50,
             }
         )
         logging.info("Saved information to dynamo table")
@@ -42,17 +44,15 @@ class Dynamodb:
         logging.info(application_id)
         try:
             response = self.table.get_item(
-                Key={
-                    'application_id': application_id
-                }
+                Key={"application_id": application_id}
             )
             logging.info("retrieved information from dynamo table")
 
         except ClientError as e:
-            print(e.response['Error']['Message'])
+            print(e.response["Error"]["Message"])
         else:
-            self.guild_items.append(response['Item'])
-            return response['Item']
+            self.guild_items.append(response["Item"])
+            return response["Item"]
 
     def delete_guild_item(self, application_id: int) -> dict:
         """Deletes the DynamoDb record based on the application id provided
@@ -65,16 +65,14 @@ class Dynamodb:
         """
         try:
             response = self.table.delete_item(
-                Key={
-                    'application_id': application_id
-                }
+                Key={"application_id": application_id}
             )
 
         except ClientError as e:
             raise e
         else:
             return response
-    
+
     def guild_item_exists_in_memory(self, application_id: int) -> dict:
         """If we have already retrieved an item before through a call to the database, this method will return that item
 
@@ -86,6 +84,5 @@ class Dynamodb:
         """
         for item in self.guild_items:
             logging.info(item["application_id"])
-            if(item["application_id"] == application_id):
+            if item["application_id"] == application_id:
                 return item
-        
