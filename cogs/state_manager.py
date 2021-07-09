38228d57672e07606523962f082c4e9c62c7448a
Boris Liao <borisliaos@gmail.com>
2021-07-08 20:21:18 -0400

    get_player function calls database and uses values

--- cogs/state_manager.py
+++ cogs/state_manager.py
@@ -1,17 +1,19 @@
 from typing import Dict
 
 from discord.ext import commands
+from discord.ext.commands.bot import Bot
 
 from config import FFMPEG_PATH
 from src.element.database import DynamoDB
 from src.music.player import Player
+from src.preinitialization import clean_chat
 
 
 class StateManager(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self, bot: Bot):
+        self.bot: Bot = bot
         self._config_db: DynamoDB = None
-        self.players: Dict[int:Player] = {}
+        self.players: Dict[int, Player] = {}
 
     @property
     def config_db(self):
@@ -27,10 +29,24 @@ class StateManager(commands.Cog):
 
         return self._config_db
 
-    def get_player(self, guild_id: int) -> Player:
+    async def get_player(self, guild_id: int) -> Player:
         if guild_id not in self.players:
-            # record = self.config_db.get_record(guild_id=guild_id)
             self.players[guild_id] = Player(FFMPEG_PATH, self.bot)
+
+            record = self.config_db.get_record(guild_id=guild_id)
+            if record:
+                if record.volume:
+                    self.players[guild_id].volume = record.volume
+                if record.command_channel_id:
+                    command_channel = self.bot.get_channel(
+                        record.command_channel_id
+                    )
+                    if command_channel:
+                        # TODO: Replace when webhooks come
+                        await clean_chat(command_channel)
+                        await self.players[guild_id].register_command_channel(
+                            command_channel
+                        )
 
         return self.players[guild_id]
 
